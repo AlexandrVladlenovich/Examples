de586be02e9789569ed68f2fc2ebbc8f4fc962c1
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-09 22:03:22 +0500

    add Example009 ProverkaKdrat

---
+++ Example009_ProverkaKvad/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите number_A");
+string number_As = Console.ReadLine() ??"";
+int number_Ai;
+Console.WriteLine("Введите number_B");
+String number_Bs = Console.ReadLine() ??"";
+int number_Bi;
+if (int.TryParse(number_As, out number_Ai))
+{
+    if (int.TryParse(number_Bs, out number_Bi))
+    {
+        if (number_Ai == number_Bi * number_Bi)
+        {
+            Console.WriteLine("Число А является квадратом числа В");
+        }
+        else 
+        {
+            Console.WriteLine("Число А не является квадратом числа В");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Error");
+    }
+}
