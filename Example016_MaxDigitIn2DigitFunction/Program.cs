5456bc9bf3cfc4d1e3b2a6115bb313e1211b1376
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-14 20:06:22 +0500

    Example016_MaxDigitIn2DigitFunction

---
+++ Example016_MaxDigitIn2DigitFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //Напишите программу, которая выводит случайное число из отрезка [10, 99]
+    // и показывает наибольшую цифру числа.
+    Console.Clear();
+    
+    int num = new Random().Next(10, 100);
+    int max = GetMaxDigitInTwoDigitNumber(num);
+    Console.WriteLine($"Максимальная цифра в числе {num} -> {max}");
+  }
+
+  public static int GetMaxDigitInTwoDigitNumber(int number)
+  {
+      int a1 = number / 10;
+      int a2 = number % 10;
+      int max = a1;
+      if (max < a2)
+      {
+          max = a2;
+      }
+      return max;
+  }
+}
